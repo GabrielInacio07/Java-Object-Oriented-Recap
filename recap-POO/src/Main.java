fc1f8ca7360a23b24b053f3d5310fd1ecfe8f68e
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-02-10 00:15:16 -0300

    Primeiro exercício de POO, criando um objeto e exibindo seus valores

--- recap-POO/src/Main.java
+++ recap-POO/src/Main.java
@@ -1,5 +1,27 @@
+import Model.Product;
+
+import java.util.Scanner;
+
 class Main{
     public static void main(String[] args) {
-        System.out.println("EX de POO");
+        System.out.println("EX01 de POO");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite nome do produto: ");
+        String nome = input.nextLine();
+
+        System.out.println("Digite o preço do produto: ");
+        double preco = Double.parseDouble(input.nextLine());
+
+        System.out.println("Digite a quantidade de produto: ");
+        int quantidade = Integer.parseInt(input.nextLine());
+
+        Product product = new Product(nome,preco,quantidade);
+
+        System.out.println("Produto: " + product.getNome());
+        System.out.println("Preço: R$" + product.getPreco());
+        System.out.println("Quantidade em estoque: " + product.getQuantidade());
+
+        input.close();
     }
 }
---
+++ recap-POO/src/Model/Product.java
@@ -0,0 +1,26 @@
+package Model;
+
+public class Product {
+
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Product(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+}
